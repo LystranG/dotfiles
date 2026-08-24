197095cdd0721dfecfb7dc31c7dbda851dff44f1
LystranG <me@lystran.com>
2026-08-24 17:54:11 +0800

    feat: update lazyvim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+local g = vim.g
 
 opt.tabstop = 4
 opt.shiftwidth = 4
+
+-- 关闭自动格式化
+g.autoformat = false
